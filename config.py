83fb0b33325b1619372e48054e75db3b369e912a
dogandavut <davut.dogan@piasis.com.tr>
2025-06-09 17:33:01 +0300

    Hassas dosya git takibinden çıkarıldı

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-DB_CONFIG = {
-    "driver": "ODBC Driver 17 for SQL Server",
-    "server": "185.84.182.228",  # Eğer SQLEXPRESS ise: "185.84.182.228\\SQLEXPRESS"
-    "database": "servis_db",
-    "username": "sa",
-    "password": "Dell1545*-"
-}
